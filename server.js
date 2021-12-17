dc804450beb5cad7ee2de1ca567cbeba499cc42e
Dylan Pruitt <dpruitt@Mini.local>
2021-12-17 13:53:32 -0500

    final commit

--- server.js
+++ server.js
@@ -5,9 +5,9 @@ const cors = require("cors");
 app.use(cors());
 
 // This file was renamed to server.js so there would be less confusion as to it's purpose.
-// the public folder is set to static 
+// the public folder is set to static
 // calling the api brings back all of the restaurants
-// calling the api/id (id = number), brings back the individual restaurant's info 
+// calling the api/id (id = number), brings back the individual restaurant's info
 
 app.use(express.static("./public")); // using the statiic directory for the served folders
 
@@ -22,7 +22,7 @@ app.get("/api", (req, res) => {
 });
 
 // default page served
-app.get("/", (req, res) => {  
+app.get("/", (req, res) => {
   res.sendFile(__dirname + "/client/public/index.html");
 });
 
